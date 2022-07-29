a58a37996c85a60d469312039b9392e34e277cfe
AhyeonLim <ahyeon31@sookmyung.ac.kr>
2022-07-29 22:03:56 +0900

    모델에 학교 추가

--- YGT/users/admin.py
+++ YGT/users/admin.py
@@ -4,11 +4,13 @@ from django.contrib.auth.models import Group
 
 class UserAdmin(admin.ModelAdmin):
     list_display = (
+        
         'user_id', 
+        'school',
         'student_id',
         'major',
         )
-    search_fields = ('user_id', 'student_id', 'major')
+    search_fields = ( 'user_id','school', 'student_id', 'major')
 
 admin.site.register(User, UserAdmin)
 admin.site.unregister(Group) # Admin페이지의 GROUP삭제
--- YGT/users/models.py
+++ YGT/users/models.py
@@ -2,9 +2,11 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 class UserManager(BaseUserManager):
-    def create_user(self, user_id, password,  student_id, major ):
+    def create_user(self,  user_id, school, password,  student_id, major ):
         user = self.model(
+          
             user_id = user_id,
+            school = school,
             major = major,
             student_id = student_id,
  
@@ -13,8 +15,8 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
     
-    def create_superuser(self, user_id, password, student_id=None,  major =None ):
-        user = self.create_user(user_id, password,  student_id, major)
+    def create_superuser(self , user_id, school, password, student_id=None,  major =None ):
+        user = self.create_user( user_id, school, password,  student_id, major)
         user.is_superuser = True
         user.is_staff = True
         user.is_admin = True
@@ -25,9 +27,10 @@ class UserManager(BaseUserManager):
 class User(AbstractBaseUser, PermissionsMixin):
     
     objects = UserManager()
-
-    user_id = models.CharField(max_length=17, verbose_name="아이디", unique=True)
+    
+    user_id = models.CharField(max_length=17, verbose_name="아이디", unique=True) 
     password = models.CharField(max_length=256, verbose_name="비밀번호")
+    school = models.CharField( max_length=24, verbose_name="학교", null=True)
     student_id = models.IntegerField(verbose_name="학번", null=True)
     major = models.CharField( max_length=24, verbose_name="학과", null=True) 
 
